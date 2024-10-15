d6a942ea864b94203dab8eace04744248bf1e20a
sangwonlee06 <sangwonlee0622@outlook.com>
2024-10-15 13:38:21 -0700

    Initial project setup

---
+++ src/test/utils.test.ts
@@ -0,0 +1,10 @@
+import {toUpperCase} from "../app/utils";
+
+
+describe('Utils test suite', () => {
+
+    test('should return uppercase', () => {
+        const result = toUpperCase('abc');
+        expect(result).toBe('ABC')
+    })
+})
