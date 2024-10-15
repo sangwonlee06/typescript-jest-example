c7898556d8d2b58a085a71b595844b131d356b0c
sangwonlee06 <sangwonlee0622@outlook.com>
2024-10-15 13:58:31 -0700

    Refactor first example unit test to follow AAA structure

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -3,8 +3,16 @@ import {toUpperCase} from "../app/utils";
 
 describe('Utils test suite', () => {
 
-    test('should return uppercase', () => {
+    test('should return uppercase of valid string', () => {
+        // arrange:
+        const sut = toUpperCase;
+        const expected = 'ABC'
+
+        // act:
+        const actual = toUpperCase('abc')
+
+        // assert:
         const result = toUpperCase('abc');
-        expect(result).toBe('ABC')
+        expect(result).toBe(expected);
     })
 })
